13355020dd439f95d6cbd133fb2032de9cf514ef
vhrustic <vhrustic1@etf.unsa.ba>
2015-06-01 01:29:02 +0200

    ko radi ne boji se gladi

--- CMShop Solution/CMeShop/DAL/ShopInitializer.cs
+++ CMShop Solution/CMeShop/DAL/ShopInitializer.cs
@@ -12,8 +12,8 @@ namespace CMeShop.DAL
         {
             var artikli = new List<CMeShop.Models.Artikal>
             {
-                new Artikal {naziv = "Ruz za usne", slika = "http://www.slike.ba/karmin.jpg", cijena = Convert.ToDecimal(2.15), proizvodjac="Ovnak", brojKupljenih=25, opis ="Odlican karmin", rokUpotrebe=DateTime.Now, zaliheStanje=120, zemljaPorijekla="BiH"},
-                new Artikal {naziv = "Sampon", slika = "http://www.slike.ba/sampon.jpg", cijena = Convert.ToDecimal(6.95), proizvodjac="Oriflame", brojKupljenih=2, opis ="Vrlo ugodnog mirisa", rokUpotrebe=DateTime.Now, zaliheStanje=10, zemljaPorijekla="BiH"}
+                new Artikal {naziv = "Ruz za usne", slika = "~/Content/Slike/karmin.png", cijena = Convert.ToDecimal(7.15), proizvodjac="Ovnak", brojKupljenih=25, opis ="Odlican karmin", rokUpotrebe=DateTime.Now, zaliheStanje=120, zemljaPorijekla="BiH"},
+                new Artikal {naziv = "Sampon", slika = "~/Content/Slike/shampon.png", cijena = Convert.ToDecimal(2.95), proizvodjac="Oriflame", brojKupljenih=2, opis ="Vrlo ugodnog mirisa", rokUpotrebe=DateTime.Now, zaliheStanje=10, zemljaPorijekla="BiH"}
             };
             artikli.ForEach(abc => context.Artikli.Add(abc));
             context.SaveChanges();
---
+++ CMShop Solution/CMeShop/ViewModels/DobavljacArtikalViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CMeShop.ViewModels
+{
+    public class DobavljacArtikalViewModel
+    {
+        public int ID { get; set; }
+        public string naziv { get; set; }
+        public decimal cijena { get; set; }
+        public string proizvodjac { get; set; }
+        public string zemljaPorijekla { get; set; }
+        public int zaliheStanje { get; set; }
+        public string opis { get; set; }
+        [Range(1, 250)]
+        public int kolicina { get; set; }
+        public bool updateovano { get; set; }
+    }
+}
